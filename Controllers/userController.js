fc07ebf307a3fbd2255cee2adcd8969f9a618c0c
Alvaro Perez Pintado <122454645+AlvaroNW@users.noreply.github.com>
2023-04-15 16:24:40 +0200

    done

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -8,6 +8,13 @@ const getUsers = async (req, res) => {
         return res.status(500).json({ message: error.message });
     }
 }
+const getUser = async (req, res) => {
+    try {
+        const user = await pool.query("SELECT * FROM users WHERE id = $1", [req.params.id]);
+        return res.status(200).json(user.rows);
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+}
 
-
-export{ getUsers }
+export{ getUsers, getUser }
