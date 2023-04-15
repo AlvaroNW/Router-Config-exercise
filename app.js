ea6f395d465bd3be19c9688a3b19bb9ab12fb52b
Alvaro Perez Pintado <122454645+AlvaroNW@users.noreply.github.com>
2023-04-15 16:17:50 +0200

    get all users

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 import Express, { Router } from "express";
 import pkg from "./DB/client.js";
+import userRoutes from "./Routes/userRoutes.js";
+
 const app = Express();
 const port = 3000;
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
+
+//middleware
+app.use('/users', userRoutes);
 
 app.listen(port, () => {
     console.log(`app listening on port ${port}`);
